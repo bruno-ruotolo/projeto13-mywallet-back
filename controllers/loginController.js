a4c6159b83884bb062cfbd312eee5cfa9726e013
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-05-05 12:02:17 -0300

    feat: login token

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
 import joi from "joi";
 import bcrypt from "bcrypt";
 import chalk from "chalk";
+import { v4 as uuid } from "uuid";
 
 import db from "../db.js"
 
@@ -13,17 +14,22 @@ export async function loginUser(req, res) {
   });
 
   const { error } = loginSchema.validate({ email, password });
-
   if (error) return res.status(422).send(error.details.map(error => error.message));
 
   try {
     const user = await db.collection("registeredUsers").findOne({ email });
 
     if (user && bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({
+        userId: user._id,
+        token
+      });
+
       delete user.password;
       delete user._id
-
       res.status(200).send(user);
+
     } else {
       res.status(400).send("Usuario/senha invalida");
     }
